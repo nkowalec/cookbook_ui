dc8be773da378a583c14a7511bd4de9ee1bfe11d
Nikodem Kowalec <n.kowalec@netis.com.pl>
2017-05-10 12:41:34 +0200

    Dodanie grafik oraz manipulacja wyglądem - LoginPage & AccountPage

--- cookbook_ui/LoginPage.xaml.cs
+++ cookbook_ui/LoginPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -29,7 +30,7 @@ namespace cookbook_ui
 
         private void SubmitBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Frame.Navigate(typeof(AccountPage));
         }
     }
 }
